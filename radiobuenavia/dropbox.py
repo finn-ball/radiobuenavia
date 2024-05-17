462638621938b6d128ec1d0db0a1bed00d3ffc02
Finn Ball <finn.ball@codifcasolutions.com>
2024-05-17 11:53:43 +0100

    feat: add extra folders

--- radiobuenavia/dropbox.py
+++ radiobuenavia/dropbox.py
@@ -9,14 +9,15 @@ CHUNK_SIZE = 4 * 1024 * 1024
 
 
 class DropBoxClient():
-    def __init__(self, app_key: str, app_secret: str, refresh_token: str, preprocess_path: str, postprocess_path: str):
+    def __init__(self, app_key: str, app_secret: str, refresh_token: str, preprocess_path: str, postprocess_archive_path: str, postprocess_soundcloud_path: str):
         self.__dbx = dropbox.Dropbox(
             app_key=app_key,
             app_secret=app_secret,
             oauth2_refresh_token=refresh_token
         )
         self.__preprocess_path = preprocess_path
-        self.__postprocess_path = postprocess_path
+        self.__postprocess_archive_path = postprocess_archive_path
+        self.__postprocess_soundcloud_path = postprocess_soundcloud_path
 
     def list_files(self, path: str) -> List[FileMetadata]:
         """List all files in the directory."""
@@ -47,9 +48,29 @@ class DropBoxClient():
         """Downloads the file."""
         self.__dbx.files_download_to_file(local_path, download_path)
 
-    def upload_file(self, local_path: str, name: str) -> None:
+    def upload_file_soundcloud(self, local_path: str, name: str) -> None:
+        remote_path = self._get_remote_path_soundcloud(name)
+        self.upload_file(local_path, remote_path)
+
+    def _get_remote_path_soundcloud(self, name: str):
+        return self._get_remote_path(self.__postprocess_soundcloud_path, name)
+
+    def _get_remote_path_archive(self, name: str):
+        return self._get_remote_path(self.__postprocess_archive_path, name)
+
+    def _get_remote_path(self, path:str, name: str):
+        return "{}/{}".format(path, name)
+
+    def copy_to_archive(self, name: str):
+        remote_path_from = self._get_remote_path_soundcloud(name)
+        remote_path_to = self._get_remote_path_archive(name)
+        self.copy(remote_path_from, remote_path_to)
+
+    def copy(self, from_path: str, to_path: str):
+        self.__dbx.files_copy_v2(from_path, to_path)
+
+    def upload_file(self, local_path: str, remote_path: str) -> None:
         """Uploads files using the session method."""
-        remote_path = "{}/{}".format(self.__postprocess_path, name)
         file_size = os.path.getsize(local_path)
         n_total = file_size / CHUNK_SIZE
         n = 0
@@ -87,7 +108,7 @@ class DropBoxClient():
     def list_files_to_process(self) -> List[FileMetadata]:
         """List all files that can be processed."""
         preproc = []
-        postproc = [f.name for f in self.list_files(self.__postprocess_path)]
+        postproc = [f.name for f in self.list_files(self.__postprocess_archive_path)]
         for f in self.list_files(self.__preprocess_path):
             if not self.rename_file(f) in postproc:
                 preproc.append(f)
